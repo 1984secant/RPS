aa8431831553b5939d97b7e4fb991ab53729a358
1984secant <127aacrooms@frhsd.com>
2026-04-24 13:57:18 -0400

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+//create an event listener for the submit button for it to, on click, remove the field and replace it with a welcome message and then display the rest of the page(?)
+let submit = document.getElementById("submit");
+
+let decide = document.getElementById("decision");
+
+decide.addEventListener('click', send);
+
+let compDecisions = ["rock","paper","scissors"]
+function send(){
+    //unknown
+}
