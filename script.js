c3e19d77cb8ced84eb3f0bbdaa58a99042699c4d
Amelia-May Crooms <127aacrooms@frhsd.com>
2026-04-29 13:00:59 -0400

    ;lSduhjvxoj jo xclk lks

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 //create an event listener for the submit button for it to, on click, remove the field and replace it with a welcome message and then display the rest of the page(?)
 let submit = document.getElementById("submit");
+let namebox = document.getElementById("player");
 
 let decide = document.getElementById("decision");
+let message = document.getElementById("welcome");
 
 decide.addEventListener('click', send);
+submit.addEventListener('click', welcome);
 
 let compDecisions = ["rock","paper","scissors"]
 function send(){
     //unknown
+}
+
+function welcome(){
+    message.innerHTML = ("WELCOME TO THE GAME, " + namebox.value);
 }
